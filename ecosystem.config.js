a2b4986bdcb0bacb750553c0bca3027c74173bea
Konano <nanoapezlk@gmail.com>
2025-06-25 10:32:21 +0800

    基础环境

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     apps: [{
-        name: 'IngressMissionArtsBot',
+        name: 'IngressMission',
         cmd: 'bot.py',
         interpreter: '/home/nano/.miniconda3/envs/bot/bin/python3',
         // autorestart: false,
